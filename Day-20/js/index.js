7e8026f6d37fd4c95e538b3197ef5feeae90f170
Ahmet can Tanrıverdi <ahmetteymen72@gmail.com>
2026-01-06 16:42:19 +0300

    .

--- Day-20/js/index.js
+++ Day-20/js/index.js
@@ -8,23 +8,28 @@ input.addEventListener("focus", () => {
 input.addEventListener("blur", () => {
     setTimeout(() => results.classList.add("d-none"), 200);
 });
+// console.log(input);
 
 document.querySelectorAll("#nameResult button").forEach(btn => {
     btn.addEventListener("click", () => {
         const targetId = btn.getAttribute("data-target");
         const targetbox = document.getElementById(targetId);
-
+// console.log(targetbox);
         document.querySelectorAll(".group-box").forEach(box => {
             box.classList.remove("active-border");
         });
+
 
         targetbox.classList.add("active-border");
     });
 });
 //!   click box hover efect 
+//      css düzgün çalışormu denemek amaçlı 
 // document.querySelectorAll(".user-card").forEach(card => {
 //   card.addEventListener("click", () => {
 //     document.querySelectorAll(".user-card").forEach(c => c.classList.remove("active"));
 //     card.classList.add("active");
 //   });
 // });
+// console.log();
+
